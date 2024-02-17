9ec375daf711fc505b1f2491ae50898e5a3f16e4
Mykola Setrakov <nikset1@gmail.com>
2024-02-17 22:36:38 +0200

    rewrote method clean and save for field "deadline" and added default value for field "is_completed"

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,8 +1,10 @@
 from typing import Any
 
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
+from django.utils import timezone
 
 
 class Position(models.Model):
@@ -50,10 +52,23 @@ class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     deadline = models.DateTimeField()
-    is_completed = models.BooleanField()
+    is_completed = models.BooleanField(default=False)
     priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def clean(self) -> None:
+        if self.deadline < timezone.now():
+            raise ValidationError("The deadline cannot be in the past.")
+
+    def save(self,
+             force_insert: bool = False,
+             force_update: bool = False,
+             using: Any | None = None,
+             update_fields: Any | None = None) -> None:
+        self.full_clean()
+        return super(Task, self) \
+            .save(force_insert, force_update, using, update_fields)
 
     def __str__(self) -> str:
         return f"{self.name} {self.deadline} {self.priority}"
