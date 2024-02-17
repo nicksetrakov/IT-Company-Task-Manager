f8d1d8b581c993ffca9a2f0e84992d721f6e11f3
Mykola Setrakov <nikset1@gmail.com>
2024-02-17 13:59:59 +0200

    created models: Position, Worker, TaskType, Task

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,60 @@
-from django.db import models
+from typing import Any
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self) -> Any:
+        return reverse("task_manager:worker-detail", kwargs={"pk": self.pk})
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.deadline} {self.priority}"
+
