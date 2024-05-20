dbfad1b7a06683eeedc15223ad0e6e5ccda994df
Mykola Setrakov <nikset1@gmail.com>
2024-05-20 22:30:38 +0300

    update settings.py and add .env.example

--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9*g=zskry)8f96-j2$$1kwd@yn97=#%^yx11nf#_z%101!n@2s"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", "django-insecure-9*g=zskry)8f96-j2$$1kwd@yn97=#%^yx11nf#_z%101!n@2s"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["task-manager.com", "localhost", "127.0.0.1"]
 
