9bade4d71f9de9c73f9a0bf2425ad924670518c4
Mykola Setrakov <nikset1@gmail.com>
2024-05-21 07:44:26 +0300

    set open_browser False to run_local_server

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -21,7 +21,7 @@ def get_credentials() -> Credentials | None:
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=0, open_browser=False)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     return creds
