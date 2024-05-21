d4a96c0e5cd6c668a08fb60c66edcd7f5478c22d
Mykola Setrakov <nikset1@gmail.com>
2024-05-21 09:40:08 +0300

    delete unused imports

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -3,7 +3,6 @@ import os
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib import get_user_credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
