0c667372a9fdcb616f1d59dab58caf9eade9fd40
Mykola Setrakov <nikset1@gmail.com>
2024-05-21 12:16:04 +0300

    update WorkerAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,29 +1,53 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Worker, Position
+from .models import Worker, Position, ActivateToken
+from django.utils.translation import gettext_lazy as _
 
 
 @admin.register(Worker)
 class WorkerAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ("position",)
-    fieldsets = UserAdmin.fieldsets + (
-        (("Additional info", {"fields": ("position",)}),)
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "username", "position")}),
         (
-            (
-                "Additional info",
-                {
-                    "fields": (
-                        "first_name",
-                        "last_name",
-                        "position",
-                    )
-                },
-            ),
-        )
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "position")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+
+@admin.register(ActivateToken)
+class ActivateTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'token', 'create_at')
+    search_fields = ('user', 'token')
 
 
 admin.site.register(Position)
