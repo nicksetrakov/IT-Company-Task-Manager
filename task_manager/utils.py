331769b78e51bd37cb4148266e9f2630c2c1897d
Mykola Setrakov <nikset1@gmail.com>
2024-05-21 09:33:05 +0300

    return get_credentials

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -27,11 +27,7 @@ def get_credentials() -> Credentials | None:
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             )
-            creds = get_user_credentials(
-                scopes=SCOPES,
-                client_id=API_AUTH_GOOGLE_OAUTH2_KEY,
-                client_secret=API_AUTH_GOOGLE_OAUTH2_SECRET,
-            )
+            creds = flow.run_local_server(port=0)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     return creds
