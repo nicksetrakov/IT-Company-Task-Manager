29fe71cd557355488759ef7c2785826cebd75dc3
Mykola Setrakov <nikset1@gmail.com>
2024-05-21 09:14:43 +0300

    refactor get_credentials

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -1,8 +1,11 @@
 import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google_auth_oauthlib import get_user_credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+
+from task_manager_service.settings import API_AUTH_GOOGLE_OAUTH2_KEY, API_AUTH_GOOGLE_OAUTH2_SECRET
 
 SCOPES = [
     "https://www.googleapis.com/auth/calendar",
@@ -18,10 +21,11 @@ def get_credentials() -> Credentials | None:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
+            creds = get_user_credentials(
+                scopes=SCOPES,
+                client_id=API_AUTH_GOOGLE_OAUTH2_KEY,
+                client_secret=API_AUTH_GOOGLE_OAUTH2_SECRET,
             )
-            creds = flow.run_local_server(port=0)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     return creds
